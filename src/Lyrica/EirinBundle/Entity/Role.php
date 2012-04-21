0d607c439cfd648db5cb5d9dc8b3ed8f73cc815b
Nate <antoine.lafouasse@me.com>
2012-04-22 00:04:27 +0200

    Generating Eirin:Role (Entity)

---
+++ src/Lyrica/EirinBundle/Entity/Role.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Lyrica\EirinBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Lyrica\EirinBundle\Entity\Role
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Lyrica\EirinBundle\Entity\RoleRepository")
+ */
+class Role
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $stage
+     *
+     * @ORM\Column(name="stage", type="string", length=255)
+     */
+    private $stage;
+
+    /**
+     * @var string $rolename
+     *
+     * @ORM\Column(name="rolename", type="string", length=255)
+     */
+    private $rolename;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set stage
+     *
+     * @param string $stage
+     */
+    public function setStage($stage)
+    {
+        $this->stage = $stage;
+    }
+
+    /**
+     * Get stage
+     *
+     * @return string 
+     */
+    public function getStage()
+    {
+        return $this->stage;
+    }
+
+    /**
+     * Set rolename
+     *
+     * @param string $rolename
+     */
+    public function setRolename($rolename)
+    {
+        $this->rolename = $rolename;
+    }
+
+    /**
+     * Get rolename
+     *
+     * @return string 
+     */
+    public function getRolename()
+    {
+        return $this->rolename;
+    }
+}
