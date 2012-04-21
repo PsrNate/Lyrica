4e69bcc1340fe790b7c6504fcb26e95698bbcac0
Nate <antoine.lafouasse@me.com>
2012-04-22 00:02:04 +0200

    Linking entities (Eirin)

--- src/Lyrica/EirinBundle/Entity/Appearance.php
+++ src/Lyrica/EirinBundle/Entity/Appearance.php
@@ -3,6 +3,9 @@
 namespace Lyrica\EirinBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Lyrica\EirinBundle\Entity\Game;
+use Lyrica\EirinBundle\Entity\Persona;
+use Lyrica\EirinBundle\Entity\Role;
 
 /**
  * Lyrica\EirinBundle\Entity\Appearance
@@ -12,6 +15,24 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Appearance
 {
+    /**
+     * @var Persona $persona
+     * @ORM\ManyToOne(targetEntity="Lyrica\EirinBundle\Entity\Persona")
+     */
+    private $persona;
+    
+    /**
+     * @var Game $game
+     * @ORM\ManyToOne(targetEntity="Lyrica\EirinBundle\Entity\Game")
+     */
+    private $game;
+    
+    /**
+     * @var Role $role
+     * @ORM\ManyToOne(targetEntity="Lyrica\EirinBundle\Entity\Role")
+     */
+    private $role;
+    
     /**
      * @var integer $id
      *
@@ -165,5 +186,65 @@ class Appearance
     public function getVeteran()
     {
         return $this->veteran;
+    }
+
+    /**
+     * Set persona
+     *
+     * @param Lyrica\EirinBundle\Entity\Persona $persona
+     */
+    public function setPersona(\Lyrica\EirinBundle\Entity\Persona $persona)
+    {
+        $this->persona = $persona;
+    }
+
+    /**
+     * Get persona
+     *
+     * @return Lyrica\EirinBundle\Entity\Persona 
+     */
+    public function getPersona()
+    {
+        return $this->persona;
+    }
+
+    /**
+     * Set game
+     *
+     * @param Lyrica\EirinBundle\Entity\Game $game
+     */
+    public function setGame(\Lyrica\EirinBundle\Entity\Game $game)
+    {
+        $this->game = $game;
+    }
+
+    /**
+     * Get game
+     *
+     * @return Lyrica\EirinBundle\Entity\Game 
+     */
+    public function getGame()
+    {
+        return $this->game;
+    }
+
+    /**
+     * Set role
+     *
+     * @param Lyrica\EirinBundle\Entity\Role $role
+     */
+    public function setRole(\Lyrica\EirinBundle\Entity\Role $role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * Get role
+     *
+     * @return Lyrica\EirinBundle\Entity\Role 
+     */
+    public function getRole()
+    {
+        return $this->role;
     }
 }
