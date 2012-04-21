201110a8ab262116fc0ec80ce8c710a58b663336
Nate <antoine.lafouasse@me.com>
2012-04-21 21:04:48 +0200

    Generating Eirin:Game (Entity)

---
+++ src/Lyrica/EirinBundle/Entity/Game.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Lyrica\EirinBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Lyrica\EirinBundle\Entity\Game
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Lyrica\EirinBundle\Entity\GameRepository")
+ */
+class Game
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
