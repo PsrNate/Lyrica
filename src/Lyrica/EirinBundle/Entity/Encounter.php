d8d9f3a8b82fedc07804be001ae38eb0a4e63fe5
Nate <antoine.lafouasse@me.com>
2012-04-24 01:17:26 +0200

    Generating Eirin:Encounter (Entity)

---
+++ src/Lyrica/EirinBundle/Entity/Encounter.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Lyrica\EirinBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Lyrica\EirinBundle\Entity\Encounter
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Lyrica\EirinBundle\Entity\EncounterRepository")
+ */
+class Encounter
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var datetime $date
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var smallint $draw
+     *
+     * @ORM\Column(name="draw", type="smallint")
+     */
+    private $draw;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param datetime $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * Get date
+     *
+     * @return datetime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set draw
+     *
+     * @param smallint $draw
+     */
+    public function setDraw($draw)
+    {
+        $this->draw = $draw;
+    }
+
+    /**
+     * Get draw
+     *
+     * @return smallint 
+     */
+    public function getDraw()
+    {
+        return $this->draw;
+    }
+}
