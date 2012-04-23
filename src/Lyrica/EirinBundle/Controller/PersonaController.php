8b6159e86169ba30574539aa7348a5504badd190
Nate <antoine.lafouasse@me.com>
2012-04-23 23:48:48 +0200

    Creating a rankings page (Eirin)

--- src/Lyrica/EirinBundle/Controller/PersonaController.php
+++ src/Lyrica/EirinBundle/Controller/PersonaController.php
@@ -7,4 +7,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PersonaController extends Controller
 {
+    /**
+     * Displays rankings of every persona, with only global Elo
+     */
+    public function rankingsAction()
+    {
+        // Short for Entity Manager
+        $em = $this->getDoctrine()->getEntityManager();
+        // Short for Persona Repository
+        $pr = $em->getRepository('LyricaEirinBundle:Persona');
+        
+        // Configure the query
+        $criteria = array();
+        $order = array('elo' => 'desc');
+        
+        // Fetch personae
+        $argts['personae'] = $pr->findBy($criteria, $order);
+        
+        // Then return a response
+        $tpl_path = 'LyricaEirinBundle:Persona:rankings.html.twig';
+        return $this->render($tpl_path, $argts);
+    }
 }
