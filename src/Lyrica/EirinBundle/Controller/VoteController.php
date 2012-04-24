e94f7b60e2d748562dff42da2ecd602fe9a253c7
Nate <antoine.lafouasse@me.com>
2012-04-25 00:54:19 +0200

    Creating Vote Controller (Eirin)

---
+++ src/Lyrica/EirinBundle/Controller/VoteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Lyrica\EirinBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class VoteController extends Controller
+{
+    /**
+     * Displays the vote page with two personae and their appearances
+     */
+    public function indexAction()
+    {
+        
+    }
+    /**
+     * Processes an encounter
+     */
+    public function matchAction()
+    {   
+    }
+}
