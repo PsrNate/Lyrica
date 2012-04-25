aa1bbc03fca57d2f72f9fca7d9b482583eca2a31
Nate <antoine.lafouasse@me.com>
2012-04-26 01:54:21 +0200

    Creating options action (Eirin:Vote)

--- src/Lyrica/EirinBundle/Controller/VoteController.php
+++ src/Lyrica/EirinBundle/Controller/VoteController.php
@@ -14,10 +14,41 @@ class VoteController extends Controller
     {
         
     }
+    
     /**
      * Processes an encounter
      */
     public function matchAction()
     {   
+    }
+    
+    /**
+     * Displays the voting options
+     */
+    public function optionsAction()
+    {
+        // Fetching excluded games
+        $argts['ex_games'] = $this->get('session')->get('ex_games');
+        
+        if (empty($argts['ex_games']))
+        {
+            $argts['ex_games'] = array();
+        }
+        
+        // Fetching all games
+        // Getting the Repository
+        $em = $this->getDoctrine()->getEntityManager();
+        $gr = $this->getRepository('LyricaEirinBundle:Game');
+        
+        // Configuring request
+        $crt = array();
+        $ord = array('opus', 'ASC');
+        
+        // Fetching
+        $argts['games'] = $gr->findBy($crt, $ord);
+        
+        // Then rendering
+        $tpl_path = 'LyricaEirinBundle:Vote:options.html.twig';
+        return $this->render($tpl_path, $argts);
     }
 }
