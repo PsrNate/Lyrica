7f70acffc46b77a246bba6e941df37b96a23b0ab
Nate <antoine.lafouasse@me.com>
2012-04-29 16:54:38 +0200

    Implementing the findOpponents method (Eirin:PersonaRepository)

--- src/Lyrica/EirinBundle/Entity/PersonaRepository.php
+++ src/Lyrica/EirinBundle/Entity/PersonaRepository.php
@@ -12,4 +12,44 @@ use Doctrine\ORM\EntityRepository;
  */
 class PersonaRepository extends EntityRepository
 {
+    /**
+     * Fetches two opponents, up to user options
+     *
+     * @param array $excluding the game ops excluded by user
+     * @return array
+     */
+    public function findOpponents($excluding = array())
+    {
+        // Initial query : find valid ids
+        $qb = $this->createQueryBuilder();
+        $qb->select('p.id');
+        $qb->from('Persona', 'p');
+        $qb->innerJoin('Game', 'g');
+        
+        // Exclusions
+        foreach ($excluding as $opus)
+        {
+            $qb->where('g.opus != '.$opus);
+        }
+        
+        // Applying query
+        $ids = $qb->getQuery()->getScalarResult();
+        
+        // Then picking two random ids
+        $idsLength = count($ids);
+        //First
+        $results[0] = rand(0, $idsLength);
+        // Second
+        do
+            {
+                $results[1] = rand(0, $idsLength);
+            }
+        while ($results[1] == $results[0]);
+        
+        // Finally, return the personae
+        $results[0] = $this->find($results[0]);
+        $results[1] = $this->find($results[1]);
+        
+        return $results;
+    }
 }
