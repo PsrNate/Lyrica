85e58c6d195a725785c66cc5f70082ee1f7b94bf
Nate <antoine.lafouasse@me.com>
2012-05-08 17:42:42 +0200

    Generating Eirin:Avatar entity

---
+++ src/Lyrica/EirinBundle/Entity/Avatar.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Lyrica\EirinBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Lyrica\EirinBundle\Entity\Avatar
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Lyrica\EirinBundle\Entity\AvatarRepository")
+ */
+class Avatar
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string $path
+     *
+     * @ORM\Column(name="path", type="string", length=255)
+     */
+    private $path;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+}
