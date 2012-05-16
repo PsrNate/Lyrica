22229f0dfcf5d3d00d56b10f2cd6a4bb0da635ad
Nate <antoine.lafouasse@me.com>
2012-05-16 16:58:56 +0200

    Adding showAction to Eirin:Appearance (controller)

--- src/Lyrica/EirinBundle/Controller/AppearanceController.php
+++ src/Lyrica/EirinBundle/Controller/AppearanceController.php
@@ -3,7 +3,29 @@
 namespace Lyrica\EirinBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Lyrica\EirinBundle\Entity\Appearance;
 
 class AppearanceController extends Controller
 {
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+    
+    /**
+     * Shows one appearance
+     */
+    public function showAction(Appearance $app)
+    {
+        $argts['entity'] = $app;
+        
+        $deleteForm = $this->createDeleteForm($app->getId());
+        $argts['delete_form'] = $deleteForm->createView();
+        
+        $tpl_path = 'LyricaEirinBundle:Appearance:show.html.twig';
+        return $this->render($tpl_path, $argts);
+    }
 }
