110a20d1528aa65a6db636473bc8a0c75ddca1fe
Nate <antoine.lafouasse@me.com>
2012-05-22 20:46:52 +0200

    Creating Eirin:Appearance:addRole action

--- src/Lyrica/EirinBundle/Controller/AppearanceController.php
+++ src/Lyrica/EirinBundle/Controller/AppearanceController.php
@@ -7,6 +7,40 @@ use Lyrica\EirinBundle\Entity\Appearance;
 
 class AppearanceController extends Controller
 {
+    /**
+     * Links a role to the appearance
+     */
+    public function addRoleAction($app_id)
+    {
+        // Getting the appearance
+        $em = $this->getDoctrine()->getEntityManager();
+        $ar = $em->getRepository('LyricaEirinBundle:Appearance');
+        $app = $ar->find($app_id);
+        // Template argument
+        $argts['app'] = $app;
+        
+        // Role options (form)
+        $opt['class'] = 'LyricaEirinBundle:Role';
+        $opt['empty_value'] = false;
+        $opt['query_builder'] =
+            function(EntityRepository $er)
+            {
+                return $this->createQueryBuilder('r')
+                            ->orderBy('r.order', 'ASC');
+            };
+        
+        // Then the form
+        $form = $this->createFormBuilder($app);
+        $form->add('id', 'hidden');
+        $form->add('role', 'entity', $opt);
+        // Template argument
+        $argts['form'] = $form->createView();
+        
+        // And finally render everything
+        $tpl_path = 'LyricaEirinBundle:Appearance:addRole.html.twig';
+        return $this->render($tpl_path, $argts);
+    }
+    
     private function createDeleteForm($id)
     {
         return $this->createFormBuilder(array('id' => $id))
