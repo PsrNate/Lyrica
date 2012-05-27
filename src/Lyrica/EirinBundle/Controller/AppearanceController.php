2ac2ce89cd6cb0cc5950c0dda0adbb09449b7c20
Nate <antoine.lafouasse@me.com>
2012-05-28 00:32:58 +0200

    Creating an action to link roles and apps (Eirin)

--- src/Lyrica/EirinBundle/Controller/AppearanceController.php
+++ src/Lyrica/EirinBundle/Controller/AppearanceController.php
@@ -9,7 +9,7 @@ use Lyrica\EirinBundle\Form\AppearanceRoleType;
 class AppearanceController extends Controller
 {
     /**
-     * Links a role to the appearance
+     * A form to link a role to an appearance
      */
     public function addRoleAction($app_id)
     {
@@ -39,6 +39,33 @@ class AppearanceController extends Controller
             ->add('id', 'hidden')
             ->getForm()
         ;
+    }
+    
+    /**
+     * Links a role to the appearance, then forwards to the appearance's view
+     */
+    public function linkRoleAction()
+    {
+        // Request, Entity Repository
+        $request = $this->get('request');
+        $em = $this->getDoctrine()->getEntityManager();
+        $ar = $em->getRepository('LyricaEirinBundle:Appearance');
+        
+        // Get the form and its data
+        $form = $this->createFormBuilder(new AppearanceRoleType);
+        $form->bindRequest($request);
+        $data = $form->getData();
+        
+        // Get entities and link them
+        $app = $ar->find($data['app_id']);
+        $app->addRole($data['role']);
+        $em->persist($app);
+        $em->flush();
+        
+        // Finally redirect
+        $opt['id'] = $data['app_id'];
+        $url = $this->generateUrl('persona_show', $opt);
+        return $this->redirect($url);
     }
     
     /**
